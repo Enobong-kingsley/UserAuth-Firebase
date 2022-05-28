a99cd494398040660634e1e8660c75e95c58d0b8
Enobong-kingsley <enobongndedde@gmail.com>
2022-05-28 15:05:29 +0100

    UploadimageToCloud

---
+++ app/src/main/java/com/example/loginandout/UploadProfilePiActiviity.java
@@ -0,0 +1,14 @@
+package com.example.loginandout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadProfilePiActiviity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_profile_pi_activiity);
+    }
+}
